e45e7c9d87f6bd1b90ab830393af78eded57c47a
alan ng <alan ng>
2017-03-26 20:32:49 +0800

    slounge v1

---
+++ Assets/movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CnControls;
+
+public class movement : MonoBehaviour {
+
+    Rigidbody2D rbody;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 movement_vector = new Vector2(CnInputManager.GetAxis("Horizontal"), CnInputManager.GetAxis("Vertical"));
+
+        if (movement_vector != Vector2.zero)
+        {
+            anim.SetBool("is_walking", true);
+            anim.SetFloat("input_x", movement_vector.x);
+            anim.SetFloat("input_y", movement_vector.y);
+        } else
+        {
+            anim.SetBool("is_walking", false);
+        }
+
+        rbody.MovePosition(rbody.position + movement_vector*Time.deltaTime);
+    }
+}
+
---
+++ Assets/player_follow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_follow : MonoBehaviour {
+
+    public Transform target;
+    public float mSpeed = 0.01f;
+    Camera mycam;
+
+	// Use this for initialization
+	void Start () {
+        mycam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mycam.orthographicSize = (Screen.height / 100f) / 4f; //responsive scaling
+
+        if (target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, mSpeed) + new Vector3(0,0,-10);
+        }
+
+	}
+}
